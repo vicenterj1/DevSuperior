ed124166848a6af9935c335a09685eb3d6595267
vicenterj1 <vicenterj1@gmail.com>
2022-12-04 18:45:51 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,27 +13,28 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	@Transactional (readOnly = true)
+
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
-		
-		//List<CategoryDTO> listDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+
+		// List<CategoryDTO> listDTO = list.stream().map(x -> new
+		// CategoryDTO(x)).collect(Collectors.toList());
+
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
-	@Transactional (readOnly = true)
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("CV:Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("CV:Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -41,6 +44,20 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try  {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch ( EntityNotFoundException e) {
+			throw new ResourceNotFoundException("CV: Id not found " + id);
+		}
+		
 	}
 
 }
