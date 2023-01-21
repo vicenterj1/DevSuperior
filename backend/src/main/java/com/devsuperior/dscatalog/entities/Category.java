eda98faf032275b86256134094141240713c9ba6
vicenterj1 <vicenterj1@gmail.com>
2023-01-21 12:07:51 -0300

    01-24 Product entity, SQL SCRIPT - Mapeamento N-N

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -10,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
-import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
