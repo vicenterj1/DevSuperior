a60c3794d64f3967f98cb048d2ed9d91f7b79d84
vicenterj1 <vicenterj1@gmail.com>
2023-02-05 16:31:31 -0300

    02-18 Testando outro cenario do delete

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -22,4 +23,16 @@ public class ProductRepositoryTests {
 		Optional<Product> result = repository.findById(exintingId);
 		Assertions.assertFalse(result.isPresent());
 	}
+	
+	@Test
+	public void deleteShouldTrhowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+		
+		long nonExisting = 1000L;
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, 
+				() -> {
+						repository.deleteById(nonExisting);
+				});
+	}
+	
 }
