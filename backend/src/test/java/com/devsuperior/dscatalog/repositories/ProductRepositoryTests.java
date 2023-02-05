cf848b9db5ce1c7061d5cdbd0747a772197ee5fc
vicenterj1 <vicenterj1@gmail.com>
2023-02-05 16:39:32 -0300

    02-19 Fixtures no JUnit, BeforeEach

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,10 +16,19 @@ public class ProductRepositoryTests {
 
 	@Autowired
 	private ProductRepository repository;
+
+	private long exintingId;
+	private long nonExisting;
 	
+	@BeforeEach
+	void setUp() throws Exception {
+		exintingId = 1L;
+		nonExisting = 1000L;
+	}
+
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		long exintingId = 1L;
+		
 		repository.deleteById(exintingId);
 		Optional<Product> result = repository.findById(exintingId);
 		Assertions.assertFalse(result.isPresent());
@@ -26,8 +36,6 @@ public class ProductRepositoryTests {
 	
 	@Test
 	public void deleteShouldTrhowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
-		
-		long nonExisting = 1000L;
 		
 		Assertions.assertThrows(EmptyResultDataAccessException.class, 
 				() -> {
