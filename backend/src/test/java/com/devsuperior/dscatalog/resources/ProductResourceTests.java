bdab46ad8c1cfb92cab01f92f108d50d2c9dd424
vicenterj1 <vicenterj1@gmail.com>
2023-02-08 21:45:06 -0300

    02-31 Legibilidade e negociação de conteúdo

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -13,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.services.ProductService;
@@ -45,4 +47,15 @@ public class ProductResourceTests {
 		
 		mockMvc.perform(get("/products")).andExpect(status().isOk());
 	}
+	
+	@Test
+	public void findAllShouldReturnPage_2() throws Exception {
+		
+		ResultActions result = mockMvc.perform(get("/products")
+									.accept(MediaType.APPLICATION_JSON)
+				);
+				
+		result.andExpect(status().isOk());
+	}
+
 }
