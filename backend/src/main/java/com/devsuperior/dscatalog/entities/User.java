87b0ee27d865981be665d2039c5be45f3582ea6c
vicenterj1 <vicenterj1@gmail.com>
2023-04-15 17:53:26 -0300

    03-06 CRUD de usuários PARTE 1

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +28,8 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 	
-	@ManyToMany
+// força que sempre venha a lista de roles do usuário	
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 			joinColumns = @JoinColumn(name = "user_id"),
 			inverseJoinColumns = @JoinColumn(name ="role_id"))
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,109 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional(readOnly = true)
+	public List<UserDTO> findAll() {
+		List<User> list = repository.findAll();
+		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("CV:Entity not found"));
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		copyDTOtoEntity(dto, entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try  {
+			User entity = repository.getOne(id);
+			copyDTOtoEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		}
+		catch ( EntityNotFoundException e) {
+			throw new ResourceNotFoundException("CV: Id not found " + id);
+		}
+		
+	}
+
+	public void delete(Long id) {
+			try {
+			repository.deleteById(id);
+			}
+			catch (EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException("CV - Id not found " + id);
+			}
+			catch (DataIntegrityViolationException e) {
+				throw new DatabaseException("CV - Integrity violation");
+			}
+		
+		
+	}
+
+	public Page<UserDTO> findAllPaged(PageRequest pageRequest) {
+		Page<User> list = repository.findAll(pageRequest);
+		return list.map(x -> new UserDTO(x));
+	}
+	
+	public Page<UserDTO> findAllPaged(Pageable pageable) {
+		Page<User> list = repository.findAll(pageable);
+		return list.map(x -> new UserDTO(x));
+	}
+
+
+	private void copyDTOtoEntity(UserDTO dto, User entity) {
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setEmail(dto.getEmail());
+		
+		entity.getRoles().clear();
+		for (RoleDTO roleDto : dto.getRoles()) {
+			Role role = roleRepository.getOne(roleDto.getId());
+			entity.getRoles().add(role);
+		}
+	};
+	
+
+}
