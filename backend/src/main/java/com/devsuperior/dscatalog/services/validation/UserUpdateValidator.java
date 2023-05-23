836e9fc3bf35b489db8e584054deb43ced26a9eb
vicenterj1 <vicenterj1@gmail.com>
2023-05-23 10:16:01 -0300

    03-15 UserUpdateValidator PARTE 2

--- backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
@@ -2,19 +2,24 @@ package com.devsuperior.dscatalog.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
-import com.devsuperior.dscatalog.dto.UserDTO;
-import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.resources.exceptions.FieldMessage;
 
-public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private UserRepository repository;
@@ -24,14 +29,18 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
 	}
 
 	@Override
-	public boolean isValid(UserDTO dto, ConstraintValidatorContext context) {
+	public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+		
+		// amarelo porque não está reconhecendo o tipo
+		var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		long userId = Long.parseLong(uriVars.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
 		
 		User user = repository.findByEmail(dto.getEmail());
-		if (user != null) {
+		if (user != null && userId != user.getId()) {
 			list.add(new FieldMessage("email", "Email já existe."));
 		}
 		
