94716b5840d6f87dd84c8ef6db7388a891e3dee5
vicenterj1 <vicenterj1@gmail.com>
2023-07-02 16:20:32 -0300

    03-24 Externalizando configuração, variáveis de ambiente

--- backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -12,9 +12,11 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @PropertySource(value = "classpath:application.properties")
 public class AppConfig {
 	
+	
 	@Value("${jwt.secret}")
 	private String jwtSecret;
 
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
