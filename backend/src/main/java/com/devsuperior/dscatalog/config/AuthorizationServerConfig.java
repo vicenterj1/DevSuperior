e7ce93c7bf1e5ea46a042ed1f4f662f4d40c3311
vicenterj1 <vicenterj1@gmail.com>
2023-07-04 11:04:06 -0300

    03-25 Adicionando informações ao token

--- backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.devsuperior.dscatalog.components.JwtTokenEnhancer;
 
 @Configuration
 @EnableAuthorizationServer //indica que estamos implmentando o Server
@@ -38,6 +43,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private JwtTokenEnhancer tokenEnhancer;
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		// nome do metodo
@@ -57,10 +65,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		TokenEnhancerChain chain = new TokenEnhancerChain();
+		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer));
+		
 		// quem vai autorizar e qual vai ser o formato do token
 		endpoints.authenticationManager(authenticationManager)
 			.tokenStore(tokenStore)	//quais os objetos ressponsáveis por processar
-			.accessTokenConverter(accessTokenConverter); 
+			.accessTokenConverter(accessTokenConverter)
+			.tokenEnhancer(chain);
 	}
 
 	
